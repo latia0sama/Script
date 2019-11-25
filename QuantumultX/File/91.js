ce88e34888280b15ecdcbcbe9ecbd6bb5db5a760
latia0sama <58099027+latia0sama@users.noreply.github.com>
2019-11-25 17:41:58 +0800

    Update 91.js

--- QuantumultX/File/91.js
+++ QuantumultX/File/91.js
@@ -6,7 +6,7 @@ QX:
 ^https?:\/\/.+\.(my10api|(.*91.*))\.(com|tips|app|xyz)(:\d{2,5})?\/api.php$ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/91.js
 */
 /**
- * @supported 你的设备ID 4A169E23574D
+ * @supported 4A169E23574D
  */
 var body = $response.body;
 var obj = JSON.parse(body);
